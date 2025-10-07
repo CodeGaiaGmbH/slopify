d3945ddcb1a6d3cd1d95bd4d87a11397a9880200
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2025-10-07 15:31:28 +0200

    More robust

--- lib/ticket.py
+++ lib/ticket.py
@@ -1,35 +1,15 @@
 import sys
 import os
 import configparser
+import subprocess
 import llm
 import utils
 import json
 
 import jira
 
-PROMPT_TEMPLATE = """
-# Persona
-You are a senior software developer.
 
-# Task
-List all files that are related to this changes.
-
-## Ticket title
-{title}
-
-## Ticket description
-```
-{description}
-```
-
-# Project overview
-```
-{project_map}
-```
-"""
-
-
-PROMPT2_TEMPLATE = """
+PROMPT_IMPLEMENT_TEMPLATE = """
 # Persona
 You are a senior software developer.
 
@@ -47,6 +27,45 @@ Implement the ticket
 ## Notes
 - Use the writeFile tool to write the files.
 """
+
+# PROMPT_PLAN_TEMPLATE = """
+# # Task
+# Implement the ticket
+#
+# # Project overview
+# ```
+# {project_map}
+# ```
+#
+# ## Ticket title
+# {title}
+#
+# ## Ticket description
+# {description}
+# """
+
+
+PROMPT_ASK_TEMPLATE = """
+# Task
+{ask}
+
+# Project overview
+```
+{project_map}
+```
+
+## Ticket title
+{title}
+
+## Ticket description
+{description}
+"""
+
+
+# Fake
+def writeFile(file: str, content: str) -> None:
+    """write file"""
+    print(f"writeFile({file}, ...) (Fake)")
 
 
 def load():
@@ -70,30 +89,100 @@ def load():
     return j.issue(ticket_id)
 
 
-def implement(issue):
+def get_relevant_files_ask(issue, ask):
     model = llm.get_model("claude-3.5-haiku")
-    prompt = PROMPT_TEMPLATE.format(
+    prompt = PROMPT_ASK_TEMPLATE.format(
+        ask=ask,
         project_map=utils.get_project_map(),
         title=issue.fields.summary,
         description=issue.fields.description.strip(" \n"),
     )
+    response = model.prompt(prompt, schema=llm.schema_dsl("file", multi=True))
 
-    response = model.prompt(
-        prompt,
-        schema=llm.schema_dsl("file", multi=True),
-    )
-    files = json.loads(response.text())
+    print()
+    print(ask)
+    try:
+        response_json = json.loads(response.text())
+        files = [i["file"] for i in response_json["items"]]
+    except TypeError as exc:
+        print(f"Error: {exc} - {response.text()}", file=sys.stderr)
+        return []
+    print(files)
+    return files
+
+
+# def get_relevant_files(issue):
+#     """
+#     Pretend to implement the ticket with cheaper model to gather relevant files.
+#     """
+#     model = llm.get_model("claude-3.5-haiku")
+#     prompt = PROMPT_PLAN_TEMPLATE.format(
+#         project_map=utils.get_project_map(),
+#         title=issue.fields.summary,
+#         description=issue.fields.description.strip(" \n"),
+#     )
+#     conversation = model.conversation()
+#     response = conversation.chain(
+#         prompt,
+#         tools=[utils.readFiles, writeFile],
+#     )
+#     print(response.text())
+#     files = set()
+#     for conversation in conversation.responses:
+#         for tc in conversation.tool_calls():
+#             if tc.name == "readFiles":
+#                 files.update(tc.arguments["files"])
+#             elif tc.name == "writeFile":
+#                 files.add(tc.arguments["file"])
+#     print(files)
+#     return list(sorted(files))
+
+
+def files_to_fragments(files):
     fragments = []
-    for item in files["items"]:
-        # print(item["file"])
+    for file in files:
         try:
-            content = utils.readFile(item["file"])
+            content = utils.readFile(file)
         except FileNotFoundError:
-            fragments.append(f"# File not found: {item['file']}")
+            fragments.append(f"# File not found: {file}")
         else:
-            fragments.append(f"# {item['file']}\n\n{content}")
+            fragments.append(f"# {file}\n\n{content}")
+    return fragments
 
-    prompt = PROMPT2_TEMPLATE.format(
+
+def implement(issue):
+    files = set()
+    files.update(
+        get_relevant_files_ask(
+            issue,
+            "Which new files must be created for this ticket?",
+        )
+    )
+    files.update(
+        get_relevant_files_ask(
+            issue,
+            "Which files are relevant to the ticket?",
+        )
+    )
+    files.update(
+        get_relevant_files_ask(
+            issue,
+            "Which files one should not forget about when implementing the ticket?",
+        )
+    )
+    files.update(
+        get_relevant_files_ask(
+            issue,
+            "Which files should be studied to understand the ticket?",
+        )
+    )
+    files.update(
+        get_relevant_files_ask(
+            issue,
+            f"I forgot files to look into for the ticket. So far we have these: {', '.join(files)}",
+        )
+    )
+    prompt = PROMPT_IMPLEMENT_TEMPLATE.format(
         title=issue.fields.summary,
         description=issue.fields.description.strip(" \n"),
     )
@@ -101,13 +190,7 @@ def implement(issue):
     model = llm.get_model("claude-sonnet-4.5")
     response = model.chain(
         prompt,
-        # schema=llm.schema_dsl("file, content", multi=True),
-        fragments=fragments,
+        fragments=files_to_fragments(sorted(files)),
         tools=[utils.writeFile],
     )
     response.text()
-
-    # files_content = json.loads(response.text())
-    # for item in files_content["items"]:
-    #     breakpoint()
-    #     utils.writeFile(item["file"], item["content"])
